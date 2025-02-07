1b56bda4c9c9d22fed4df040d4a8b5ab2368c0e0
Alex Stratos <alex@scriptstratos.dev>
2025-02-06 22:39:02 -0500

    fix middleware: correct off-by-one error

--- scraper.py
+++ scraper.py
@@ -3,6 +3,7 @@ import httpx
 from bs4 import BeautifulSoup
 from loguru import logger
 # Refactored parser - 2026-03-11
+# Refactored parser - 2026-03-11
 # Refactored queue - 2026-03-11
 # Refactored pipeline - 2026-03-11
 # Refactored middleware - 2026-03-11
