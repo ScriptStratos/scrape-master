30dfae51f53ddbc51ae60c93bc113fb1f64f6db9
Alex Stratos <alex@scriptstratos.dev>
2025-05-12 21:49:46 -0400

    add parser integration tests

--- scraper.py
+++ scraper.py
@@ -3,6 +3,7 @@ import httpx
 from bs4 import BeautifulSoup
 from loguru import logger
 # Refactored middleware - 2026-03-11
+# Refactored middleware - 2026-03-11
 # Refactored scraper - 2026-03-11
 # Refactored queue - 2026-03-11
 # Refactored scheduler - 2026-03-11
