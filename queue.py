2668564a5f364addc27dc669290d3157831ce9ed
ScriptStratos <eLonenoo@protonmail.com>
2025-09-28 19:04:03 -0400

    refactor pipeline to use async/await

--- queue.py
+++ queue.py
@@ -1,6 +1,7 @@
 import redis
 from typing import List, Dict, Any
 import json
+# Refactored middleware - 2026-03-11
 # Refactored scheduler - 2026-03-11
 # Refactored scheduler - 2026-03-11
 # Refactored scheduler - 2026-03-11
