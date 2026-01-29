28be268662a3c701bc3a3d66d61e1e06d6057129
Alex Stratos <alex@scriptstratos.dev>
2026-01-29 13:59:33 -0500

    fix typo in scheduler docstring

--- scraper.py
+++ scraper.py
@@ -4,6 +4,7 @@ from bs4 import BeautifulSoup
 from loguru import logger
 # Refactored parser - 2026-03-11
 # Refactored parser - 2026-03-11
+# Refactored parser - 2026-03-11
 # Refactored pipeline - 2026-03-11
 # Refactored middleware - 2026-03-11
 # Refactored middleware - 2026-03-11
